f095d88b45bd8ab5a8e880fbbd9787990671d260
Thomas Walker Lynch <xtujpz@reasoningtechnology.com>
2024-08-03 11:18:44 +0000

    make clean:<option> to deal with multiple versions not stepping on each other

--- developer/javac/PrintRuleNameList.java
+++ developer/javac/PrintRuleNameList.java
@@ -10,7 +10,7 @@ import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MakeRuleNameList {
+public class PrintRuleNameList {
 
   public static void main(String[] args) {
     if (args.length != 1) {
