c40a0bdcc6ea7ee2e02acc9a19a60bbabaab6c31
Thomas Walker Lynch <xtujpz@reasoningtechnology.com>
2024-09-04 15:41:18 +0000

    introduces R_MAKE

--- developer/executor/makefile-project.mk
+++ developer/executor/makefile-project.mk
@@ -12,78 +12,88 @@ MAKEFLAGS += -r
 
 $(info makefile: $(MAKEFILE_LIST))
 $(info project_MAKECMDGOALS: $(MAKECMDGOALS))
+$(info project_MAKE: $(MAKE))
+R_MAKE = $(MAKE) -$(MAKEFLAGS) -f $(MAKEFILE_LIST)
+
 
 #================================================================================
 # Custom make targets
 #
+#  `make` in this section will be the `executor/make` script, due to `executor`
+#  being first in the path .. debugging .. this should be a variable from
+#  the env_build ...
 
 project: $(EXECUTOR_IN_FPL)
 PrintRuleNameList: $(EXECUTOR_IN_DIR)/PrintRuleNameList
 
-
 #-----------------------------------------------
 # Arithmetic
 
-ANTLR_OUT_Arithmetic_FPL $(shell ANTLR_OUT_FL Arithmetic -path $(ANTLR_OUT_DIR))
+ANTLR_OUT_Arithmetic_FPL:= $(shell ANTLR_OUT_FL Arithmetic -path $(ANTLR_OUT_DIR))
 Arithmetic_Echo:\
-  $(ANTLR_OUT_Arithmetic_FPL)
+  $(ANTLR_OUT_Arithmetic_FPL)\
   $(JAVA_COMP_IN_PRIMARY_DIR)/Arithmetic_Echo_PrintVisitor.java
 	@if [ -z "$(ANTLR_OUT_Arithmetic_FPL)" ]; then \
 	  echo "variable ANTLR_OUT_Arithmetic_FPL empty."; \
 	  exit 1; \
 	fi
-	make $(EXECUTOR_IN_DIR)/Arithmetic_Echo
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Echo
 
 Arithmetic_Echo__Test:\
-  $(ANTLR_OUT_Arithmetic_FPL)
+  $(ANTLR_OUT_Arithmetic_FPL)\
   $(JAVA_COMP_IN_PRIMARY_DIR)/Arithmetic_Echo_PrintVisitor.java
 	@if [ -z "$(ANTLR_OUT_Arithmetic_FPL)" ]; then \
 	  echo "variable ANTLR_OUT_Arithmetic_FPL empty."; \
 	  exit 1; \
 	fi
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Echo__Test
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Echo__Test
 
 Arithmetic_Syntax:\
-  $(ANTLR_OUT_Arithmetic_FPL)
+  $(ANTLR_OUT_Arithmetic_FPL)\
   $(JAVA_COMP_IN_PRIMARY_DIR)/Arithmetic_Syntax_PrintVisitor.java
 	@if [ -z "$(ANTLR_OUT_Arithmetic_FPL)" ]; then \
 	  echo "variable ANTLR_OUT_Arithmetic_FPL empty."; \
 	  exit 1; \
 	fi
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Syntax
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Syntax
 
 Arithmetic_Syntax__Test:\
-  $(ANTLR_OUT_Arithmetic_FPL)
+  $(ANTLR_OUT_Arithmetic_FPL)\
   $(JAVA_COMP_IN_PRIMARY_DIR)/Arithmetic_Syntax_PrintVisitor.java
 	@if [ -z "$(ANTLR_OUT_Arithmetic_FPL)" ]; then \
 	  echo "variable ANTLR_OUT_Arithmetic_FPL empty."; \
 	  exit 1; \
 	fi
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Syntax__Test
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/Arithmetic_Syntax__Test
 
 #-----------------------------------------------
 #  GQL_20240412
 
 ANTLR_OUT_GQL_20240412_FPL := $(shell ANTLR_OUT_FL GQL_20240412 -path $(ANTLR_OUT_DIR))
-GQL_20240412_Syntax: $(ANTLR_OUT_GQL_20240412_FPL) $(JAVA_COMP_IN_PRIMARY_DIR)/GQL_20240412_Syntax_PrintVisitor.java
+GQL_20240412_Syntax:\
+  $(ANTLR_OUT_GQL_20240412_FPL)\
+  $(JAVA_COMP_IN_PRIMARY_DIR)/GQL_20240412_Syntax_PrintVisitor.java
 	@if [ -z "$(ANTLR_OUT_GQL_20240412_FPL)" ]; then \
 	  echo "variable ANTLR_OUT_GQL_20240412_FPL empty."; \
 	  exit 1; \
 	fi
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/GQL_20240412_Syntax
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/GQL_20240412_Syntax
 
-GQL_20240412_Syntax__Test: $(ANTLR_OUT_GQL_20240412_FPL) $(JAVA_COMP_IN_PRIMARY_DIR)/GQL_20240412_Syntax_PrintVisitor.java
+GQL_20240412_Syntax__Test: \
+  $(ANTLR_OUT_GQL_20240412_FPL) \
+  $(JAVA_COMP_IN_PRIMARY_DIR)/GQL_20240412_Syntax_PrintVisitor.java
 	@if [ -z "$(ANTLR_OUT_GQL_20240412_FPL)" ]; then \
 	  echo "variable ANTLR_OUT_GQL_20240412_FPL empty."; \
 	  exit 1; \
 	fi
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/GQL_20240412_Syntax__Test
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/GQL_20240412_Syntax__Test
+
 
 TerminalToCategory: 
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/TerminalToCategory
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/TerminalToCategory
 
 GrammarSplitter: 
-	$(BIN_MAKE) $(EXECUTOR_IN_DIR)/GrammarSplitter
+	$(R_MAKE) $(EXECUTOR_IN_DIR)/GrammarSplitter
 
 #-----------------------------------------------
 # Compile all the .java files.
